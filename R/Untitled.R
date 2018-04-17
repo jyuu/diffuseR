0b3f69897418b949f4946bfb943ad1d07a4a42da
jyuu <jyu21@ncsu.edu>
2018-04-17 12:27:21 -0400

    now working with changing

--- R/Untitled.R
+++ R/Untitled.R
@@ -4,7 +4,6 @@
 # load --------------------------------------------------------------------
 #library(R.matlab)
 
-
 # pics --------------------------------------------------------------------
 #file <- "/Users/jcahoon/Downloads/Sec_6.1/Patch/"
 #ex <- "npatchJ August.mat"
@@ -26,6 +25,70 @@ ep.nov = 0.161087707605546;
 num_eig = 20;
 # diffusion time 
 t = 1;
+
+# try it with built in R package ------------------------------------------
+library(diffusionMap)
+D.X <- as.matrix(dist(X))
+d.X <- diffuse(D.X, eps.val= ep.octc, neigen = num_eig, t = t)
+rm(D.X)
+rm(X)
+# now apply to second one
+D.Y <- as.matrix(dist(Y))
+d.Y <- diffuse(D.Y, eps.val= ep.nov, neigen = num_eig, t = t)
+# now apply the paper stuff
+VX <- d.X$X
+EX <- diag(d.X$eigenvals)
+VY <- d.Y$X
+EY <- diag(d.Y$eigenvals)
+
+# omap function -----------------------------------------------------------
+omap <- function(DMs, Vs, Ve){
+  O <- t(Vs) %*% Ve 
+  DM_e <- DMs %*% O
+  return(DM_e)
+}
+# their stuff
+if (t < Inf){
+  DMX = VX %*% (EX^t);
+  DMY_I <- VY %*% (EY^t)
+  DMY <- omap(DMY_I,VY,VX);
+}
+# change map 
+if (t < Inf){
+  chg_map = sqrt(apply((DMX-DMY)^2, 1, sum));
+} else {
+  joyce <- (VX[,1]-VY[,1])^2
+  joyce <- VX[,1] * VY[,1]*norm(as.matrix(VX[,1]-VY[,1]),type="2")^2
+  joyce <-(VX[,1]-VY[,1])^2 + VX[,1] * VY[,1]*norm(as.matrix(VX[,1]-VY[,1]),type="2")^2
+  chg_map <- sqrt((VX[,1]-VY[,1])^2 + VX[,1] * VY[,1]*norm(as.matrix(VX[,1]-VY[,1]),type="2")^2);
+}
+
+chg_map = matrix(chg_map,100,100);
+
+# display
+# Display exponent for visualization
+if (t < Inf){
+  alpha = (1/2000)*t + 0.5*(1-1/1000);
+}else{
+  alpha = 1;
+}
+
+# Plot
+# imagesc(chg_map.^alpha);
+# axis('image');
+# after!! 
+image(chg_map^alpha, axes=FALSE) # NO CHANGE DETECTED!!
+image(chg_map^.1, axes=FALSE) # NO CHANGE DETECTED!!
+
+# lets compare to before 
+octc <- X[,1] # one spectrum
+oct.pic <- matrix(octc, 100, 100)
+image(oct.pic, axes=FALSE)
+nov <- Y[,1] # one spectrum
+nov.pic <- matrix(nov, 100, 100)
+image(nov.pic, axes=FALSE)
+
+
 # make the symmetric diffusion kernals 
 D.X <- as.matrix(dist(X))
 K.X <- exp((-D.X^2)/ep.octc^2) # kernal
